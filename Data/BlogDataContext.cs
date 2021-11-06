783669190aff41f5fb97d440f22d99bdbdfde20b
Vially Israel Lopes Aragão <viallyisrael@hotmail.com>
2021-11-06 14:52:58 -0300

    Context configured

---
+++ Data/BlogDataContext.cs
@@ -0,0 +1,20 @@
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data{
+
+    public class BlogDataContext : DbContext{
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Category> Posts { get; set; }
+        //public DbSet<Category> PostTags { get; set; }
+        public DbSet<Category> Roles { get; set; }
+        public DbSet<Category> Tags { get; set; }
+        public DbSet<Category> Users { get; set; }
+        //public DbSet<Category> UserRoles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options){
+            options.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=123MudaR");
+        }
+    }
+}
