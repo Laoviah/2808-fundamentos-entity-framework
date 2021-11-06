16ec5e8b92ba8e01b905a18e1d987880b5e1f3a6
Vially Israel Lopes Aragão <viallyisrael@hotmail.com>
2021-11-06 15:47:35 -0300

    Category maped

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,12 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Blog.Models{
 
     // [Id] INT NOT NULL IDENTITY(1, 1),
     // [Name] VARCHAR(80) NOT NULL,
     // [Slug] VARCHAR(80) NOT NULL,
+    [Table("Categories")]
     public class Category{
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public int UserId { get; set; }
-        public int RoleId { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(80)]
+        [Column("Name", TypeName = "NVARCHAR")]
+        public int Name { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(80)]
+        [Column("Name", TypeName = "VARCHAR")]
         public string Slug { get; set; } ="";
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,31 +8,7 @@ namespace Blog{
 
         static void Main(string[] args){
 
-            using(var context = new BlogDataContext()){
-
-                // var tag = new Tag{Name="NET", Slug = "ASP.NET"};
-                // context.Tags.Add(tag);
-                // context.SaveChanges();
-
-                // var tag = context.Tags.FirstOrDefault(x => x.Id ==1);
-                // tag.Name = ".NET";
-                // tag.Slug = "dotnet";
-
-                // context.Update(tag);
-                // context.SaveChanges();
-
-                // var tag = context.Tags.FirstOrDefault(x => x.Id ==1);
-                // context.Remove(tag);
-                // context.SaveChanges();
-
-                // var tags = context.Tags.
-                //                     AsNoTracking().
-                //                     ToList();
-
-                // foreach(var tag in tags){
-                //     Console.WriteLine(tag.Name);
-                // }
-            }
+            using(var context = new BlogDataContext()) {};
         }
     }
 }
